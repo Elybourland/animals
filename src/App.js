37de0306b85483723e1021abb80492c3877d74a7
Elybourland <JEBourland@yahoo.com>
2022-12-29 16:58:57 -0800

    adding css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './App.css';
 import AnimalShow from './AnimalShow';
 
 function getRandomAnimal() {
@@ -21,9 +22,9 @@ const App = () => {
   });
 
   return (
-    <div>
+    <div className='app'>
       <button onClick={handleClick}>Add Animal</button>
-      <div>{renderedAnimals}</div>
+      <div className='animal-list'>{renderedAnimals}</div>
     </div>
   )
 }
